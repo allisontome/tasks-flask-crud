f673767071649c1c0093c4fe2844b3d8b9ddf916
allison tomé <allison.tome.silva@hotmail.com>
2024-03-30 15:31:00 -0300

    CRUD concluído

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ def read_task(id):
     for t in tasks:
           if t.id == id:
                return jsonify(t.to_dict())
+                
     
-    return jsonify({"mensage": "Atividade não encontrada"}), 404
+    return jsonify({"message": "Atividade não encontrada"}), 404
 
 
 @app.route('/tasks/<int:id>', methods=['PUT'])
@@ -43,15 +44,32 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
     
     if(task == None):
-        return jsonify({"mensage": "Atividade não encontrada" }), 404
+        return jsonify({"message": "Atividade não encontrada" }), 404
     
     data = request.get_json();
 
     task.update(data)
 
     return "sucesso"
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    
+    if not task:
+        return jsonify({"message": "não foi possível encontrar a atividade"}), 404
+
+    tasks.remove(task)
+
+    return jsonify({"message": "atividade removida com sucesso"})
 
 if __name__ == "__main__":
         app.run(debug=True)
